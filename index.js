08c726929f7619dbbcfd56fefadf1e45eef1abf4
feyy <felixyuebin@gmail.com>
2017-11-01 19:14:33 +0800

    upgrade code

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ function getEmojiChoices(types) {
  * @return {Array} Return an array of `inquier.js` questions
  * @private
  */
+let appendsNames = [];
+
 function createQuestions(res) {
   const config = res.pkg.config || {}
-  const emojiConfig = config['cz-emoji'] || {}
+  const emojiConfig = config['cz-emoji-xg'] || {};
+  const appends = emojiConfig.appends || [];
+  appendsNames = appends.map(function (item) { return item.name;});
 
   return [
     {
@@ -35,11 +39,11 @@ function createQuestions(res) {
       name: 'type',
       message: "Select the type of change you're committing:",
       choices: getEmojiChoices(emojiConfig.types || types)
-    },
+    }].concat(appends, [
     {
       type: emojiConfig.scopes ? 'list' : 'input',
       name: 'scope',
-      message: 'Specify a scope:',
+      message: 'Specify the affected scope (pages or files ...):',
       choices: emojiConfig.scopes && [{ name: '[none]', value: '' }].concat(emojiConfig.scopes)
     },
     {
@@ -57,7 +61,7 @@ function createQuestions(res) {
       name: 'body',
       message: 'Provide a longer description:'
     }
-  ]
+    ]);
 }
 
 /**
@@ -67,16 +71,33 @@ function createQuestions(res) {
  * @return {String} Formated git commit message
  */
 function format(answers) {
+
   // parentheses are only needed when a scope is present
   const scope = answers.scope ? '(' + answers.scope.trim() + ') ' : ''
 
+  // concat appends
+  let appends = '';
+  if (appendsNames.length) {
+    appendsNames.forEach(function (item) {
+      appends += '[' + answers[item] + ']'
+    });
+  }
+
   // build head line and limit it to 100
-  const head = truncate(answers.type + ' ' + scope + answers.subject.trim(), 100)
+  let commit = truncate(answers.type + appends + scope + answers.subject.trim(), 100)
 
   // wrap body at 100
-  const body = wrap(answers.body, 100)
+  const body = wrap(answers.body, 100);
+  if (body) {
+    commit += '\n\n' + body
+  }
 
-  return (head + '\n\n' + body)
+  const issues = wrap(answers.issues, 100);
+  if (issues) {
+    commit += '\n\n' + issues
+  }
+
+  return (commit)
 }
 
 /**
