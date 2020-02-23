f09a0c903754a6de4f60e0567f96c807201b7ba8
ngryman <ngryman@gmail.com>
2020-02-23 16:20:14 +0100

    :bug: handle non-existing .czrc local files correctly (#34)

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function loadConfig() {
       .then(getConfig)
 
   const readFromLocalCzrc = () =>
-    readPkg().then(res => readFromCzrc(`${path.dirname(res.path)}/.czrc`))
+    readPkg().then(res => (res ? readFromCzrc(`${path.dirname(res.path)}/.czrc`) : null))
 
   const readFromGlobalCzrc = () => readFromCzrc(homeDir('.czrc'))
 
